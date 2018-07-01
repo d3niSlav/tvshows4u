9e0344334cfe528918ac6f7dd1fc94b8c3c4b135
Denislav Enchev <deni.b.enchev@gmail.com>
2018-07-01 16:32:38 +0300

    Mobile menu added.

--- src/app/web/header/main-navigation/main-navigation.component.ts
+++ src/app/web/header/main-navigation/main-navigation.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-navigation.component.scss']
 })
 export class MainNavigationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/web/header/mobile-menu/mobile-menu.component.ts
+++ src/app/web/header/mobile-menu/mobile-menu.component.ts
@@ -1,17 +1,41 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-mobile-menu',
   templateUrl: './mobile-menu.component.html',
   styleUrls: ['./mobile-menu.component.scss']
 })
-export class MobileMenuComponent {
-  isHamburgerMenuOpen = false;
+export class MobileMenuComponent implements OnInit, OnDestroy{
+  isHamburgerMenuOpen: boolean = false;
+  isUserAuthenticated: boolean = false;
+  subscription: Subscription;
 
-  constructor() {
+  constructor(private authService: AuthService , private router: Router) { }
+
+  ngOnInit() {
+    this.isUserAuthenticated = this.authService.isUserLoggedIn();
+    this.subscription = this.authService.userAuthenticationChanged
+      .subscribe(
+        (user: any) => {
+          this.isUserAuthenticated = !!user;
+        }
+      );
   }
 
   openHamburgerMenu() {
     this.isHamburgerMenuOpen = !this.isHamburgerMenuOpen;
+  }
+
+  onLogout() {
+    sessionStorage.removeItem('jwtToken');
+    this.authService.logout();
+    this.router.navigate(['']);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
